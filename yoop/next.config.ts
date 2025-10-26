25a24aeccf359f37ed3db663a52e4ec43c0871f5
Fornar <apogore@sfedu.ru>
2025-10-26 12:12:55 +0300

    next config

--- yoop/next.config.ts
+++ yoop/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [{ source: "/api/:path*", destination: "http://backend:80/:path*" }];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
