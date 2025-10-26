0ca5a2d1fd0fb103065bc039ba6660bc07f1440a
Fornar <apogore@sfedu.ru>
2025-10-26 16:00:42 +0300

    check build net

--- yoop/next.config.ts
+++ yoop/next.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [{ source: "/api/:path*", destination: "http://backend/:path*" }];
+    return [
+      { source: "/api/:path*", destination: "http://backend:8080/:path*" },
+    ];
   },
 };
 
